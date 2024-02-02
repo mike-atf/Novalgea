4550d3cdbd4d0ada7e00c933cd778fc33a9d7dd5
Michael Luckmann <michael.luckmann@saps-medical.co.uk>
2024-02-02 15:06:19 +0000

    Initial Commit

---
+++ Novalgea/NovalgeaApp.swift
@@ -0,0 +1,20 @@
+//
+//  NovalgeaApp.swift
+//  Novalgea
+//
+//  Created by aDev on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct NovalgeaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
